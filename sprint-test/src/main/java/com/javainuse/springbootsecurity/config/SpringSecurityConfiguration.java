f95be07f0be95c47bed9d3b547bd97d6d6f72a48
Ajit Angaj <ajitangaj@gmail.com>
2022-09-03 06:57:54 +0530

    Adding JWT authentication
    Adding JWT authentication

---
+++ sprint-test/src/main/java/com/javainuse/springbootsecurity/config/SpringSecurityConfiguration.java
@@ -0,0 +1,50 @@
+package com.javainuse.springbootsecurity.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfiguration {
+	
+	@Autowired
+	private CustomJwtAuthenticationFilter customJwtAuthenticationFilter;
+	
+	@Autowired
+	private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;	
+	
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+	
+	@Bean
+	public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
+		http.csrf().disable()
+		.authorizeRequests().antMatchers("/helloadmin").hasRole("ADMIN")
+		.antMatchers("/hellouser").hasAnyRole("USER","ADMIN")
+		.antMatchers("/authenticate").permitAll().anyRequest().authenticated()
+		.and().exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).
+		and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).
+		and().addFilterBefore(customJwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+		return http.build();
+	}
+
+
+
+}
